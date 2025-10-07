a261d584f5aa185ffab2362c7d5c963dbfc9a88b
Raj Verma <rohitrajverma6202@gmail.com>
2025-10-08 00:42:23 +0530

    Initial commit: Add Amazon company profile landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const navLinks = document.querySelectorAll('.nav-item a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
+
